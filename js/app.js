dee58d64bfe60be8fc43ab41e624f099c178df31
Rafal Kapusta <rfkapusta@gmail.com>
2019-11-01 16:11:00 +0100

    improved js, animations and added web browsers prefixes

--- js/app.js
+++ js/app.js
@@ -1,62 +1,68 @@
 // html elements
-let gameElements = document.querySelectorAll('.game__bubble');
 let gameContainer = document.querySelector('.container');
 let gameScore = document.querySelector('.game__menu__score .score__counter');
 let timeCounter = document.querySelector('.game__menu__score .time__counter');
-let totalScore = document.querySelector('.total__score');
-let counter = 0;
-//bubblesArray = [];
+let start = document.querySelector('#start');
+let bubblesArray = [];
+let numOfBubbles = 10;
 
 // defining border of container which bubbles can't cross
+
 let width = gameContainer.offsetWidth;
 let height = gameContainer.offsetHeight;
-// console.log(height);
-// console.log(width);
 
-// funcja tworząca bombelki
+// bubbles constructor function
 
 function Bubble(x,y){
     this.x = x;
     this.y = y;
 }
-/*
-Bubble.prototype.typeOfBubble = function () {
-    let typeOfBubble = Math.floor(Math.random()*3);
-    console.log(typeOfBubble);
-    if(typeOfBubble === 0) {
-        this.classList.add('game__bubble--small');
-    }else if(typeOfBubble === 1) {
-        this.classList.add('game__bubble--medium');
-    }else {
-        this.classList.add('game__bubble--big');
-    }*/
-// initial bubble
-// let x = Math.random() * (width - 150);
-// let y = Math.random() * (height - 150);
-// bubblesArray.push(new Bublle(x, y));
 
-function TrueFalseCheck(a,b){
+// prototype function drawing bubble type (size)
+Bubble.prototype.type = function type() {
+    let type = Math.floor(Math.random() * 3);
+    if (type === 0) {
+        return this.type = 'game__bubble--small';
+    } else if (type === 1) {
+        return this.type = 'game__bubble--medium';
+    } else {
+        return this.type = 'game__bubble--big';
+    }
+};
+
+// prototype function drawing bubble color
+
+Bubble.prototype.color = function color() {
+    let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    return this.color = randomColor;
+};
+
+// object needed to keep true or false returned from bubbleCheck function
+function TrueFalseCheck(a,b,c){
     this.a = a;
     this.b = b;
+    this.c = c;
 }
 
-//losowanie współrzędnych bąbla
-function bubblesCheck(width, height) {
+//drawing bubble position. bubbles can't be positioned one on top another
+function bubbleCheck(width, height) {
     let tempArr = [];
     bubblesArray = [];
-    while (bubblesArray.length < 10) {
+    while (bubblesArray.length < numOfBubbles) {
+        // 150 is width and height of the largest bubble
         x = Math.random() * (width - 150);
-        //console.log(x);
         y = Math.random() * (height - 150);
-        //console.log(y);
         let nextBubble = new Bubble(x,y);
+        nextBubble.type();
+        nextBubble.color();
         for(let i = 0; i < bubblesArray.length; i++) {
             let a = Boolean(Math.abs(bubblesArray[i].x - nextBubble.x) > 150);
             let b = Boolean(Math.abs(bubblesArray[i].y - nextBubble.y) > 150);
-            tempArr.push(new TrueFalseCheck(a,b));
+            let c = Boolean(parseInt(bubblesArray[i].color) - parseInt(nextBubble.color));
+            tempArr.push(new TrueFalseCheck(a,b,c));
         }
         if(tempArr.some(function(elem) {
-            return (elem.a === false && elem.b === false)
+            return (elem.a === false && elem.b === false && elem.c === false)
         })) {
             tempArr = [];
         } else {
@@ -69,75 +75,98 @@ function bubblesCheck(width, height) {
     return bubblesArray;
 }
 
-// console.log(randomX(width));
-//console.log(bubblesCheck(width,height));
+//creating bubbles in HTML
+function createBubbles(arr) {
+    for(let i = 0; i < arr.length; i++){
+        let div = document.createElement('div');
+        div.classList.add('game__bubble');
+        div.classList.add(arr[i].type);
+        gameContainer.appendChild(div);
+    }
+}
+createBubbles(bubbleCheck(width,height));
 
-//losowanie początkowego położenia oraz koloru
-bubblesCheck(width,height);
-gameElements.forEach(function(element, index) {
-    let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-    element.style.left = bubblesArray[index].x + 'px';
-    element.style.top = bubblesArray[index].y + 'px';
-    element.style.backgroundColor = randomColor;
+//selecting all created bubbles
+let gameElements = document.querySelectorAll('.game__bubble');
+
+gameElements.forEach(function(elem) {
+   elem.style.opacity = '0';
 });
 
-// gameElements.forEach(function (bubble,index) {
-//     console.log(bubble.offsetLeft,index);
-//     // console.log(bubble.offsetTop,index);
-// })
+start.addEventListener('click', function (e) {
+    gameElements.forEach(function(elem) {
+        elem.style.opacity = '1';
+    });
+    gameScore.innerText++;
+    setInterval(timeCounterInterval,1000);
+    start.style.visibility='hidden';
+});
 
+//drawing bubbles initial color
+gameElements.forEach(function(element, index) {
+    element.style.backgroundColor = bubblesArray[index].color;
+});
 
-// function which in interval changes position and color of bubbles
+// changing bubbles position and color every second
 let timeInterval = setInterval(function(){
-    bubblesCheck(width,height);
+    bubbleCheck(width,height);
     //console.log(gameElements);
     gameElements.forEach(function(element, index) {
-        let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        //let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
         element.style.left = bubblesArray[index].x + 'px';
         element.style.top = bubblesArray[index].y + 'px';
-        element.style.backgroundColor = randomColor;
+        //element.style.backgroundColor = bubblesArray[index].color;
     })
 },1000);
 
-// clearInterval(timeInterval);
-
-// time counter, conditions to stop time counter, remove eventListeners when timer stops and count total score
-
-let timeCounterInterval = setInterval(function(){
+// time count, game stops after 60 seconds
+function timeCounterInterval() {
     timeCounter.innerText++;
 
-    if(counter == 10){
-        clearInterval(timeCounterInterval);
-        clearInterval(timeInterval);
-        gameElements.forEach(function(element) {
+    if (timeCounter.innerText === '60') {
+        gameElements.forEach(function (element) {
             element.removeEventListener('click', gameElementClick)
         });
         gameContainer.removeEventListener('click', gameContainerClick);
-        //totalScore.innerText = Number(gameScore.innerText) + Number(timeCounter.innerText);
+        clearInterval(timeInterval);
+        clearInterval(timeCounterInterval);
+        alert('Game Over! Your score is ' + gameScore.innerText);
     }
-},1000)
-
-// clearInterval(timeInterval);
-
-// adding eventListener for bubbles
-gameElements.forEach(function(element){
-    element.addEventListener('click', gameElementClick)
-})
-
-//adding eventListener fo game container (- poits)
-gameContainer.addEventListener('click', gameContainerClick)
+}
 
 //function handler for bubble eventListener
 function gameElementClick(e) {
     e.stopPropagation();
-    gameScore.innerText++;
-    this.style.display = 'none';
-    counter++;
-    return counter;
+    let score = 0;
+    if(this.classList[1] === 'game__bubble--small') {
+        score += 3;
+        gameScore.innerText = (parseInt(gameScore.innerText) + score).toString();
+    }else if(this.classList[1] === 'game__bubble--medium'){
+        score += 2;
+        gameScore.innerText = (parseInt(gameScore.innerText) + score).toString();
+    }else {
+        score += 1;
+        gameScore.innerText = (parseInt(gameScore.innerText) + score).toString();
+    }
+    this.classList.remove(this.classList[1]);
+    this.classList.add(bubblesArray[0].type);
+    this.style.backgroundColor = bubblesArray[0].color;
 }
+
+// adding eventListener for bubbles
+gameElements.forEach(function(element){
+    element.addEventListener('click', gameElementClick)
+});
 
 //function handler for gameContainer eventListener
 function gameContainerClick(e) {
     gameScore.innerText--;
 }
+
+//adding eventListener fo game container (- poits)
+gameContainer.addEventListener('click', gameContainerClick);
+
+
+
+
 
